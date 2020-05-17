f5589e5e3a03361985d2081620a03beb32c9f2dd
dheeraj poonia <2017pietcsdheeraj035@poornima.org>
2020-05-17 16:42:27 +0000

    done

--- mathematics.h
+++ mathematics.h
@@ -1,4 +1,4 @@
-ll gcd(ll x, ll y) {
++ll gcd(ll x, ll y) {
   return y ? gcd(y, x % y) : x;
 }
 
@@ -7,4 +7,54 @@ ll lcm(ll a, ll b)
    return a * b / gcd(a, b);
 }
 
-hi
+/*
+m1. O(n) 
+m2. O(n/2)
+m3. O(sqrt(n))   TRIAL DIVISION METHOD
+    if n is not prime than some no a is divide n and we get result b : b=n/a -> a*b = n then over n lies between a & b somewhere
+*/
+bool checkPrime(int n){
+  if(n<=1){
+    return false;
+  }
+  else{
+    for(int i=2; i<=sqrt(n); i++){
+      if(n%i==0){
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+
+/*
+m1. O(n*n)
+m2. O(n*n/2)
+m3. O(n*sqrt(n))
+m4. O(n)	SIEVE OF ERATOSTHENES
+*/
+vector<int> findPrime(int n){
+	vector<int> res;
+	if(n<2){
+		return res;
+	}
+	else{
+		vector<bool> prime(n,true);
+		prime[0]=false;
+		prime[1]=false;
+		for(int i=2; i<=n; i++){
+			for(int multiple=i+i; multiple<=n; multiple*=i){
+				prime[multiple]=false;
+			}
+		}
+		
+		for(int i=1; i<n; i++){
+			if(prime[i-1]){
+				res.pb(i);
+			}
+		}
+
+	}
+	return res;
+}
---
+++ test.cpp
@@ -0,0 +1,14 @@
+//#include "bit_manipulation.h"
+//#include "array_manipulation.h"
+#include "macro.h"
+#include "mathematics.h"
+
+int main()
+{
+    px(checkPrime(21));
+    px(checkPrime(7));
+
+    pvec(findPrime(10));
+
+    return 0;
+}
