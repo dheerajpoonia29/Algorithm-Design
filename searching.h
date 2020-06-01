324af7f00c3731e0ff501ff4d44d271902f8e4ca
dheeraj poonia <2017pietcsdheeraj035@poornima.org>
2020-06-01 07:12:36 +0000

    6/1/2020

--- searching.h
+++ searching.h
@@ -1,139 +1,155 @@
-#include "..\tools\macros.h"
-using namespace std;
 
-class Searching{
 
-public:
+int linearSearch(int arr[], int n, int sk){
+	int ki=-1;
+	for(int i=0; i<n; i++){
+			if(arr[i]==sk){
+				ki = i;
+				break;
+			}
+	}
+	return ki;
+}
 
-    int* arr
-    int n, i, mid, key_index=-1, search_key=-1;
-    int total_iteration = 1;
-    string algo_name = "",time_complexity = "", space_complexity = "";
-    double time_taken = 0.0;
 
-    Searching(int myarr[], int myn, int mykey){
-        n = myn;
 
-        arr = new int[n];
+int binarySearch(vector<int> arr, x){
+	int a = 0, b = n - 1;
+	while (a <= b) {
+		int k = (a + b) / 2;
+		if (arr[k] == x) {
+			// x found at index k
+			return k;
+		}
+		if (arr[k] > x)
+			b = k - 1;
+		else a = k + 1;
+	}
+}
 
-        arr = myarr;
+/*
+int recursiveBinarySearch(vector<int> arr, int sk)
+{
+	int low=0, int high=arr.size()-1, mid;
+	int ki=-1;
 
-        search_key = mykey;
-    }
+	if(low>high){
+			return -1;
+	}
+	int mid=(low+high)/2;
+	if(a[mid]==sk){
+			return mid;
+	}
+	else if(a[mid]<sk)
+	{
+			low=mid+1;
+	}
+	else
+	{
+			high=mid-1;
+	}
+	return binarySearch(low, high, key);
+}
 
-    void linearSearch(){
+int lowerBound(int low, int high){
 
-        algo_name = "linearSearch";
+	while(low<=high){
+						total_iteration++;
+						int mid = low + (high - low) / 2;
+						if(arr[mid]<sk){
+								low=mid+1;
+						}
+						else if(arr[mid]>=sk){
+								high=mid-1;
+								ki=mid;
+						}
+		}
 
-        i=0;
-        fo(i,n){
-            total_iteration++;
-            if(arr[i]==search_key){
-                key_index = i;
-                break;
-            }
-        }
-    }
+		return 0;
 
-    int binarySearch(int low, int high){
+}
 
-        algo_name = "binarySearch";
+int upperBound(int low, int high)
+{
 
-        while(low<=high){
-                total_iteration++;
-                mid = low + (high - low) / 2;
-                if(arr[mid]==search_key){
-                    key_index = mid;
-                }
+		algo_name = "upperBound";
 
-                if(arr[mid]<search_key){
-                        low=mid+1;
-                }
-                else{
-                        high=mid-1;
-                }
-        }
+		while(low<=high){
+						total_iteration++;
+						int mid = low + (high - low) / 2;
+						if(arr[mid]<=sk){
+								low=mid+1;
+								ki=mid;
+						}
+						else if(arr[mid]>sk){
+								high=mid-1;
+						}
+		}
+}
 
-        return 0;
-    }
+-------other implementation------------
+void lower&upperBound(int arr[], int target){
+	int n = A.size();
+	int i = 0, j = n - 1;
+	int start = -1, end = -1;
 
-    int lowerBound(int low, int high){
+	// FIND FIRST -> LOWER BOUND
+	while (i < j)
+	{
+			int mid = (i + j) /2;
+			if (A[mid] < target) i = mid + 1;
+			else j = mid;
+	}
+	if (A[i] != target) return 0; // target not exit
 
-        algo_name = "lowerBound";
+	start = i;  <- LB
 
-        while(low<=high){
-                total_iteration++;
-                int mid = low + (high - low) / 2;
-                if(arr[mid]<search_key){
-                    low=mid+1;
-                }
-                else if(arr[mid]>=search_key){
-                    high=mid-1;
-                    key_index=mid;
-                }
-        }
+	// FINDLAST   -> UPPER BOUND
+	j = n - 1;  // We don't have to set i to 0 the second time.
+	while (i < j)
+	{
+			int mid = (i + j) /2 + 1;   // Make mid biased to the right
+			if (A[mid] > target) j = mid - 1;  
+			else i = mid;
+	}
+	end = j;    <- UB
+}
+*/
 
-        return 0;
+int findSqrt(int n){
+	int low = 1, high = n, ans;
+	long long mid;
+	while(low<=high){
+		mid = low+(high-low)/2;
+		if(mid*mid==n)
+			return mid;
+		else if(mid*mid<n){
+			ans = mid;
+			// till now ans is sqrt but it may possbile that we find higher closet sqrt of number n
+			low = mid+1;
+		}
+		else{
+			high = mid-1;
+		}
+	}
+	return ans;
+}
 
-    }
+int countDuplicateInSortedArray(vector<int> A, int B){
+	int start_idx = lb(A,B,0,0);
+	if(A[start_idx]!=B)
+			start_idx = -1;
 
-    int upperBound(int low, int high)
-    {
+	int end_idx = ub(A,B,0,0)-1;
+	if(A[end_idx]!=B)
+			end_idx = -1;
+			
+	if(start_idx!=-1 && end_idx!=-1){
+		return end_idx-start_idx;
+	}
+	return -1;
+}
 
-        algo_name = "upperBound";
+int findPower(int base, int exponent){
 
-        while(low<=high){
-                total_iteration++;
-                int mid = low + (high - low) / 2;
-                if(arr[mid]<=search_key){
-                    low=mid+1;
-                    key_index=mid;
-                }
-                else if(arr[mid]>search_key){
-                    high=mid-1;
-                }
-        }
-    }
-
-    int inbuiltLowerBound(){
-
-        algo_name = "stl_lower_bound";
-
-        vector<int> v(arr, arr + n);
-        key_index =  lower_bound(v.begin(), v.end(), 0) - v.begin();
-    }
-
-    int inbuiltUpperBound(){
-
-        algo_name = "stl_upper_bound";
-
-        vector<int> v(arr, arr + n);
-        key_index = upper_bound(v.begin(), v.end(), n) - v.begin();
-    }
-
-    void computeTime(time_t s, time_t e){
-        time_taken = double(e-s);
-    }
-
-    void report(){
-        cout<<"_________________________"<<"\n";
-        deb1(algo_name);
-        deb3(arr,n);
-        deb2(search_key, key_index)
-        deb1(total_iteration);
-        cout<<"_________________________"<<"\n\n";
-    }
-
-    void reuse(){
-        total_iteration=0;
-        algo_name = "";
-        time_complexity = "";
-        space_complexity = "";
-        key_index = -1;
-    }
-
-    ~Searching(){
-        delete [] arr;
-        arr = NULL;
-    }
-};
+}
