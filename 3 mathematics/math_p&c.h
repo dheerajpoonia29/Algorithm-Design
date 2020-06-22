d3f2f501c908d89623b3784f85eaa57dbeb549d8
dheeraj poonia <2017pietcsdheeraj035@poornima.org>
2020-06-22 10:20:40 +0000

    6/22/2020

--- 3 mathematics/math_p&c.h
+++ 3 mathematics/math_p&c.h
@@ -51,3 +51,37 @@ public:
 	}
 
 };
+
+
+class COMBINATION{
+	void makeCombiUtil(vector<vector<int>>& ans, vector<int>& tmp, int n, int left, int k) 
+	{ 
+			// Pushing this vector to a vector of vector 
+			if(k == 0) { 
+					ans.push_back(tmp); 
+					return; 
+			} 
+		
+			// i iterates from left to n. First time 
+			// left will be 1 
+			for(int i = left; i <= n; ++i) 
+			{ 
+					tmp.push_back(i); 
+					makeCombiUtil(ans, tmp, n, i + 1, k - 1); 
+		
+					// Popping out last inserted element 
+					// from the vector 
+					tmp.pop_back(); 
+			} 
+	} 
+		
+	// Prints all combinations of size k of numbers 
+	// from 1 to n. 
+	vector<vector<int> > makeCombi(int n, int k) 
+	{ 
+			vector<vector<int> > ans; 
+			vector<int> tmp; 
+			makeCombiUtil(ans, tmp, n, 1, k); 
+			return ans; 
+	} 
+}
